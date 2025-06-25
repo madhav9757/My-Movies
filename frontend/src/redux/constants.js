1c9227b624a44db542b73df1630844e14574ec59
madhav9757 <madhavsemwal9@gmail.com>
2025-06-25 15:32:09 +0530

    fix: update API URL constants to use environment variable for BASE_URL

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,5 +1,5 @@
-export const BASE_URL = '/api';         
-export const USERS_URL = '/users';
-export const GENRE_URL = '/genre';
-export const MOVIE_URL = '/movies';
-export const UPLOAD_URL = '/upload';
+export const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';         
+export const USERS_URL = `${BASE_URL}/users`;
+export const GENRE_URL = `${BASE_URL}/genre`;
+export const MOVIE_URL = `${BASE_URL}/movies`;
+export const UPLOAD_URL = `${BASE_URL}/upload`;
