ed7e48721b1c9bbed7cccfb863fb90370b2685b4
madhav9757 <madhavsemwal9@gmail.com>
2025-06-28 13:29:28 +0530

    fix: update placeholder image URL for movie card on error

--- frontend/src/components/movie/movieCard.jsx
+++ frontend/src/components/movie/movieCard.jsx
@@ -22,7 +22,7 @@ const MovieCard = ({ movie }) => {
                         ? movie.image
                         : `${API}${movie.image}`
                 }
-                onError={(e) => (e.target.src = '/placeholder.jpg')}
+                onError={(e) => (e.target.src = 'https://media.istockphoto.com/id/1396814518/vector/image-coming-soon-no-photo-no-thumbnail-image-available-vector-illustration.jpg?s=612x612&w=0&k=20&c=hnh2OZgQGhf0b46-J2z7aHbIWwq8HNlSDaNp2wn_iko=')}
                 alt={movie.title}
             />
 
