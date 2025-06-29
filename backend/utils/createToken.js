e53c0dbdc2c6ee4c14b7a77339f0076da37c3ac4
madhav9757 <madhavsemwal9@gmail.com>
2025-06-29 17:22:03 +0530

    fix: update SameSite attribute for JWT cookie to 'None' for cross-site compatibility

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -14,7 +14,7 @@ const generateToken = (res, userId, isAdmin = false) => {
     res.cookie('jwt', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
+      sameSite: 'None',
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
   }
